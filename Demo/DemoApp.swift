2c6d7677c6647aa2fc8e21b9e08893b829270cc1
Viranchee Lotia <viranchee@outlook.com>
2020-10-14 13:00:17 +0530

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Ketan Lotia on 14/10/20.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
